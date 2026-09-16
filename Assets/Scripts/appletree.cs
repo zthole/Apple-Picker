9d61293de6c48a9dceda18d6c2a83582ef67cb45
zthole <zthole@siue.edu>
2026-09-15 21:09:00 -0500

    added functionality to apples
    - tree now spawns apples
    - apples disappear after set distance
    - tree is now kinematic

--- Assets/Scripts/appletree.cs
+++ Assets/Scripts/appletree.cs
@@ -22,8 +22,15 @@ public class appletree : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
+    {  // Start dropping apples
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
     {
-        
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appleDropDelay);
     }
 
     // Update is called once per frame
