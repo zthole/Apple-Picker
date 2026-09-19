73535a9ef87e912dbffbdcbffccdcb3b20f902bd
zthole <zthole@siue.edu>
2026-09-18 20:18:40 -0500

    Added high score tracking

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -31,6 +31,7 @@ public class Basket : MonoBehaviour {
             Destroy(collidedWith);
             // Increase the score
             scoreCounter.score += 100;
+            HighScore.TRY_SET_HIGH_SCORE(scoreCounter.score);
         }
     }
 }
