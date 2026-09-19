aad561070e1d419af72b59df4a604eeb15c91ca3
zthole <zthole@siue.edu>
2026-09-19 15:57:43 -0500

    Added 4th basket and started start screen

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -6,7 +6,7 @@ public class ApplePicker : MonoBehaviour {
     [Header("Inscribed")]
     public GameObject basketPrefab;
 
-    public int numBaskets = 3;
+    public int numBaskets = 4;
 
     public float basketBottomY = -14f;
 
