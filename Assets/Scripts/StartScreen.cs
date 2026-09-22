3e3138fd05f9534314a1e1b56ae9056c722565d2
zthole <zthole@siue.edu>
2026-09-22 16:16:21 -0500

    Finished starting screen

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // Start the game
+    public void PlayGame() {
+        SceneManager.LoadScene("_Scene_0");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
